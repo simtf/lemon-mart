2287c64de0ed8b720bdf00af1c13600363a98c7b
simtf <fabiensimonet@gmail.com>
2018-09-12 21:36:25 +0200

    Adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { InventoryModule } from './inventory/inventory.module'
 import { ManagerModule } from './manager/manager.module'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { PosModule } from './pos/pos.module'
 import { UserModule } from './user/user.module'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
